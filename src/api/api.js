2fce2ec2bc98a0143b6b90b4b633df72b8f6115b
INK <114326077+INKmin9@users.noreply.github.com>
2024-01-24 15:50:33 +0000

    ✨ feat: 질문목록 받아오기 api 수정 밒 postpage 질문 받아오기 구현, Style페이지 추가

--- src/api/api.js
+++ src/api/api.js
@@ -22,3 +22,18 @@ export const setUserData = async userData => {
     return `Error: ${error.message}`;
   }
 };
+
+const BASE_URL = 'https://openmind-api.vercel.app/3-3/subjects/'
+
+export async function getSubjectsQuestion(id, limit = 2, offset = '') {
+  const subjectId = id;
+  const query = `?limit=${limit}&offset=${offset}`;
+  const response = await fetch(
+    `${BASE_URL}/subjects/${subjectId}/questions/${query}`
+  );
+  if (!response.ok) {
+    throw new Error('질문 조회에 실패했습니다');
+  }
+  const body = await response.json();
+  return body;
+}
