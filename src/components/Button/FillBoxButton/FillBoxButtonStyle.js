4b6bae944ae5dba531bed9cfd031d337c4af458b
Jnnsu <jspp120@gmail.com>
2024-01-26 02:18:29 +0900

    🎨 chore: POST시 team 정보 누락 수정

--- src/components/Button/FillBoxButton/FillBoxButtonStyle.js
+++ src/components/Button/FillBoxButton/FillBoxButtonStyle.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import BoxButton from '../BoxButton/BoxButton';
+import BoxButton from '../BoxButton/BoxButtonStyle';
 
 export const FillButton = styled(BoxButton)`
   background: var(--Brown-40);
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const TEAM = '3-3';
+
+const BASE_URL = 'https://openmind-api.vercel.app/3-3';
+const SUBJECT_URL = `${BASE_URL}/subjects/`;
+const QUESTION_URL = `${BASE_URL}/questions/`;
+const ANSWER_URL = `${BASE_URL}/answers/`;
+
+export const API = {
+  SUBJECT: SUBJECT_URL,
+  QUESTION: QUESTION_URL,
+  ANSWER: ANSWER_URL,
+};
