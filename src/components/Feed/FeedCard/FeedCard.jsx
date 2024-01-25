b1ab88f38af917aa0594d5aa66cebfa93851ce8e
INKmin9 <mkkim9901@naver.com>
2024-01-26 03:21:19 +0900

    ✨ feat : feedcard, 섹션 컴포넌트 추가, 스타일 파일 생성

---
+++ src/components/Feed/FeedCard/FeedCard.jsx
@@ -0,0 +1,44 @@
+import {
+  BadgeButton,
+  Reaction
+} from 'components';
+import * as Styled from './FeedCardStyle';
+import { CardCreatedDate } from 'utils/CardCreatedDate';
+
+export default function (data, subjectData){
+  const {id: qusetionId, content, like, dislike, createdAt, answer} =data;
+  const [subjectName, subjectImg] = subjectData;
+
+  return (
+    <CardContainer>
+      <Header>
+        {answer? <CompleteBadge /> : <IncompleteBadge />}
+      </Header>
+      <Question>
+        <QuestionTime>
+         질문 · {CardCreatedDate(createdAt)}
+        </QuestionTime>
+        <QuestionContent>{content}</QuestionContent>
+      </Question>
+      {answer ? (
+        <AnswerContainer>
+          <ProfileImage src={subjectImg}/>
+          <AnswerBox>
+            <AnswerProfile>
+              <AnswerName>{subjectName}</AnswerName>
+              <AnswerDate>
+              {CardCreatedDate(answer['createdAt'])}
+              </AnswerDate>
+            </AnswerProfile>
+            {answer.isRejected} ? (
+              <RefuseContent>답변 거절</RefuseContent>
+            ):(
+            <AnsweredContent>{answer['content']}</AnsweredContent>
+          )}
+          </AnswerBox>
+        </AnswerContainer>
+      ) : null }
+    </CardContainer>
+  );
+
+};
