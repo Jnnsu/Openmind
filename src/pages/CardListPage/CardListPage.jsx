979991fe7845e8efb65b9c670c9c0b6e41c90cba
2zzzyoung <mzooyoungn@gmail.com>
2024-01-28 02:12:35 +0900

    🎨  feat : 스타일 컴포넌트 분리

--- src/pages/CardListPage/CardListPage.jsx
+++ src/pages/CardListPage/CardListPage.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import * as S from './CardListPageStyle';
 import DropDownButton from '../../components/DropDown/DropDownButton';
 import OutlineBoxButton from '../../components/Button/OutlineBoxButton/OutlineBoxButton';
 import Pagenaion from '../../components/Pagenation/Pagenation';
@@ -48,47 +48,47 @@ export default function CardList() {
 
   return (
     <>
-      <CardListContainer>
-        <CardListHeader>
+      <S.CardListContainer>
+        <S.CardListHeader>
           <Link to="./">
-            <Logo src="./images/logo.png" alt="로고 이미지" />
+            <S.Logo src="./images/logo.png" alt="로고 이미지" />
           </Link>
           <OutlineBoxButton onClick={handleAnswerPage}>
             답변하러 가기
           </OutlineBoxButton>
-        </CardListHeader>
-        <CardListTitleWrapper>
-          <CardListTitle>누구에게 질문할까요?</CardListTitle>
+        </S.CardListHeader>
+        <S.CardListTitleWrapper>
+          <S.CardListTitle>누구에게 질문할까요?</S.CardListTitle>
           <DropDownButton
             sortOption={sortOption}
             onSortOptionChange={handleSortOption}
           />
-        </CardListTitleWrapper>
-        <CardListMain>
+        </S.CardListTitleWrapper>
+        <S.CardListMain>
           {result.map((item, index) => (
-            <Card
+            <S.Card
               key={item.id}
               id={item.id}
               name={item.name}
               image={item.imageSource}
               questionCount={item.questionCount}
             >
-              <CardTop>
+              <S.CardTop>
                 <img src={item.imageSource} alt="" />
-                <CardNickname>{item.name}</CardNickname>
-              </CardTop>
-              <CardBottom>
-                <ReceiveQuestion>
+                <S.CardNickname>{item.name}</S.CardNickname>
+              </S.CardTop>
+              <S.CardBottom>
+                <S.ReceiveQuestion>
                   <img src="./images/Messages.svg" alt="메세지 아이콘" />
                   <div>받은 질문</div>
-                </ReceiveQuestion>
+                </S.ReceiveQuestion>
                 <div>{item.questionCount}개</div>
-              </CardBottom>
-            </Card>
+              </S.CardBottom>
+            </S.Card>
           ))}
-        </CardListMain>
-        <CardListFooter>
-          <CardPagenation>
+        </S.CardListMain>
+        <S.CardListFooter>
+          <S.CardPagenation>
             <img src="./images/Arrow-left.png" alt="왼쪽 화살표 아이콘" />
             {/* <Pagenaion /> */}
             <button>1</button>
@@ -97,177 +97,9 @@ export default function CardList() {
             <button>4</button>
             <button>5</button>
             <img src="./images/Arrow-right.png" alt="오른쪽 화살표 아이콘" />
-          </CardPagenation>
-        </CardListFooter>
-      </CardListContainer>
+          </S.CardPagenation>
+        </S.CardListFooter>
+      </S.CardListContainer>
     </>
   );
 }
-
-const CardListContainer = styled.div`
-  background: var(--Grayscale-20);
-  width: 100%;
-  height: 100vh;
-  padding: 40px 130px;
-
-  @media (min-width: 375px) and (max-width: 767px) {
-    padding: 20px 24px;
-  }
-`;
-
-const CardListHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  @media (min-width: 375px) and (max-width: 767px) {
-    flex-direction: column;
-    justify-content: center;
-  }
-`;
-
-const Logo = styled.img`
-  width: 146px;
-  height: 57px;
-`;
-
-const CardListTitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  margin: 2rem 0 3.5rem 0;
-  gap: 12px;
-
-  @media (min-width: 375px) and (max-width: 767px) {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-`;
-
-const CardListTitle = styled.h1`
-  color: var(--Grayscale-60);
-  text-align: center;
-  font-size: 40px;
-  font-weight: 400;
-  line-height: normal;
-
-  @media (min-width: 768px) {
-    font-size: 40px;
-  }
-
-  @media (min-width: 375px) {
-    font-size: 24px;
-  }
-`;
-
-const CardListMain = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  /* justify-content: space-between; */
-  gap: 20px;
-
-  @media (max-width: 949px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (max-width: 767px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-`;
-
-const Card = styled.div`
-  width: calc(25%-10px);
-  min-width: 186px;
-  max-width: 220px;
-  margin-bottom: 20px;
-  height: 187px;
-  background: var(--Grayscale-10);
-  border: 1px solid var(--Grayscale-40);
-  border-radius: 16px;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  @media (min-width: 768px) {
-    width: calc(33.33%-10px);
-  }
-
-  @media (min-width: 375px) {
-    width: 100%;
-  }
-`;
-
-const CardTop = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  img {
-    width: 60px;
-    border-radius: 50%;
-  }
-`;
-
-const CardNickname = styled.div`
-  color: var(--Grayscale-60);
-  font-size: 20px;
-  margin-top: 12px;
-`;
-
-const CardBottom = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: var(--Grayscale-40);
-
-  div {
-    font-size: 16px;
-  }
-`;
-
-const ReceiveQuestion = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16px;
-
-  img {
-    width: 18px;
-  }
-`;
-
-const CardListFooter = styled.div`
-  display: flex;
-  justify-content: center;
-  padding-top: 40px;
-`;
-
-const CardPagenation = styled.div`
-  width: 280px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  font-size: 20px;
-
-  img {
-    width: 40px;
-  }
-
-  button {
-    cursor: pointer;
-    background: none;
-    border: none;
-    flex: 1;
-    color: var(--Grayscale-40);
-
-    :active {
-      color: var(--Brown-40);
-    }
-  }
-`;
