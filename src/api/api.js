4c5f8aae633bfc02f397fc11f025398719332145
Jnnsu <jspp120@gmail.com>
2024-02-01 00:32:39 +0900

    🐛 33

--- src/api/api.js
+++ src/api/api.js
@@ -196,7 +196,10 @@ export async function deleteQuestion(questionId) {
 export const getWeatherData = async (lat, lon) => {
   try {
     const response = await fetch(
-      `${PROXY}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`,
+      'https://corsproxy.io/' +
+        encodeURIComponent(
+          'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric',
+        ),
       {
         method: 'GET',
         headers: {
