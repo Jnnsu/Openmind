6e7bf2be830bae953d2476282663c6d3b3b6b8c1
Jnnsu <jspp120@gmail.com>
2024-02-01 00:40:57 +0900

    5

--- src/api/api.js
+++ src/api/api.js
@@ -197,7 +197,7 @@ export const getWeatherData = async (lat, lon) => {
     const response = await fetch(
       'https://corsproxy.io/' +
         encodeURIComponent(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`,
+          `/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`,
         ),
       {
         method: 'GET',
